bb05036850af5118f9f4a2b3000d41c2034830a2
Joonas Ylönen <KA0Screativity@gmail.com>
2022-11-18 20:04:01 +0200

    Vk 3, harjoitus 1. Requests riippuvuus lisätty ja ohjelma modifioitu

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -1,7 +1,32 @@
+import requests
 from player import Player
 
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players"
+    response = requests.get(url).json()
+
+    #print("JSON-muotoinen vastaus:")
+    #print(response)
+
+    players = []
+
+    for player_dict in response:
+        player = Player(
+            player_dict['name'],
+            player_dict['nationality'],
+            player_dict['team'],
+            player_dict['goals'],
+            player_dict['assists']
+        )
+
+        players.append(player)
+
+    #print("Oliot:")
+    print("Players from FIN\n")
+
+    for player in players:
+        if player.nationality == "FIN":
+            print(player)
 
 if __name__ == "__main__":
     main()
--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -1,6 +1,10 @@
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, nationality, team, goals, assists):
         self.name = name
+        self.nationality = nationality
+        self.team = team
+        self.goals = goals
+        self.assists = assists
     
     def __str__(self):
-        return self.name
+        return self.name + " team " + self.team + " goals " + str(self.goals) + " assists " + str(self.assists)
